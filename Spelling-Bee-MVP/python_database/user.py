152d40df341564325a564498d0b90e2f9d170f64
s-callaerts <seppe.callaerts@gmail.com>
2025-12-16 05:33:04 +0000

    updated python users class

--- Spelling-Bee-MVP/python_database/user.py
+++ Spelling-Bee-MVP/python_database/user.py
@@ -7,3 +7,22 @@ class user (self, uid, name, email, password, grade, isTeacher = False):
         self.grade = grade
         
     def generate_uid():
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        rand_num = random.randint(1000, 9999)
+
+        return f"{timestamp}{rand_num}"
+
+    def __authorize_teacher(self, key, isTeacher):
+        self.isTeacher = True if key === validation_key else self.isTeacher = False
+
+    def save(self):
+        conn = sqlite3.connect(users.db)
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        INSERT INTO users (uid, name, email, password, grade, isTeacher)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,(self.uid, self.name, self.email, self.password, self.grade, self.isTeacher))
+
+        conn.commit()
+        conn.close()
