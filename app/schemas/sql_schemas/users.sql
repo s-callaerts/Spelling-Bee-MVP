b5c81893320c50d0d0d744ba4fb1c4c18723cc2c
s-callaerts <seppe.callaerts@gmail.com>
2026-01-18 02:27:11 +0000

    updated db initialize to include test history and content tables

--- app/schemas/sql_schemas/users.sql
+++ app/schemas/sql_schemas/users.sql
@@ -1,7 +1,11 @@
 --CREATE DATABASE IF NOT EXISTS spellingbee;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS words;
+DROP TABLE IF EXISTS test_history;
+DROP TABLE IF EXISTS test_content;
 --drop table only for testing, remove on deployment
+
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users(
     uid TEXT PRIMARY KEY,
@@ -16,6 +20,24 @@ CREATE TABLE IF NOT EXISTS users(
 CREATE TABLE IF NOT EXISTS words(
     grade INTEGER NOT NULL,
     chapter INTEGER NOT NULL,
-    japanese TEXT NOT NULL,
+    japanese TEXT NOT NULL UNIQUE,
     english TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS test_history(
+    attempt_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uid TEXT NOT NULL REFERENCES users(uid),
+    timestamp VARCHAR(60) NOT NULL,
+    grade INTEGER NOT NULL,
+    chapter INTEGER NOT NULL,
+    score INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS test_content(
+    attempt_id INTEGER REFERENCES test_history(attempt_id),
+    japanese TEXT NOT NULL,
+    english TEXT NOT NULL,
+    input TEXT NOT NULL,
+    is_correct INTEGER NOT NULL,
+    status TEXT NOT NULL,
 );
