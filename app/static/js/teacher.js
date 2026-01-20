e05ba816d162a7c09601cfbc5bdfb3986f8d330a
s-callaerts <seppe.callaerts@gmail.com>
2026-01-20 06:45:47 +0000

    remove unnecessary js files and rename dashboard file

--- app/static/js/teacher.js
+++
@@ -1,6 +0,0 @@
-db.collection("users").where("role", "==", "student").get().then(snapshot => {
-    snapshot.forEach(doc => {
-        const student = doc.data();
-        console.log(student.username, student.difficulty);
-    });
-});
